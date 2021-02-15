f0c8bb51eedb6c4ecf74edd5270f2f29eb8c567b
star-jian <phaetnzhang@gmail.com>
2021-02-14 23:07:13 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let carts = document.querySelectorAll('.add-cart')
+let products = [
+    {
+        name: ''
+    }
+]
+for (let i=0; i<carts.length; i++){
+    carts[i].addEventListener('click', ()=>{
+        cartNumbers()
+    })
+ 
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers')
+    productNumbers = parseInt(productNumbers)
+    localStorage.setItem('cartNumbers', 1)
+
+}
